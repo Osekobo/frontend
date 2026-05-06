0bd8b97703aae119770abcf07eca83641374d764
Oseko <collinsoseko2003@gmail.com>
2026-05-06 18:29:41 +0300

    Changed url again

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
-console.log("API BASE URL:", API_URL);
+const API_URL = import.meta.env.VITE_API_URL || 'https://hardware-backend-12qj.onrender.comnpm run dev';
+// console.log("API BASE URL:", API_URL);
 const api = axios.create({
   baseURL: API_URL,
   headers: {
